16804aaca85ed295138ff94b1f8433ff8bfbf66a
EliseoAntonio <89654953+EliseoAntonio@users.noreply.github.com>
2021-08-27 12:31:21 -0500

    Termine el programa 1
    espero que así haya sido como dijo que se debía hacer si no funcionaba el git

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
-
+    # inicio pidiendo los datos como en el formato
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    # ahora que tengo los datos hare la operacion
+    m = (y2 - y1) / (x2 - x1)
+    #ya guarde el resultado en una variable para no perderlo, ahora es mmento de dar el resultado
+    print("Pendiente:", m)
 
 if __name__ == '__main__':
     main()
