d6af7de1b0866cbf1977a4240fd64d0042921d51
EliseoAntonio <89654953+EliseoAntonio@users.noreply.github.com>
2021-08-27 12:49:00 -0500

    Termine programa 2
    Buenas tardes

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+    # Pedire los datos necesarios
+    mens = int(input("Dame el número de mensajes: "))
+    megas = float(input("Dame el número de megas: "))
+    minu = int(input("Dame el numero de minutos: "))
+    # hare las operaciones necesarias
+    suma = (mens + megas + minu)* 0.80
+    #Dare el resultado 
+    print("El costo mensual es:", suma)
 
 
 if __name__ == '__main__':
