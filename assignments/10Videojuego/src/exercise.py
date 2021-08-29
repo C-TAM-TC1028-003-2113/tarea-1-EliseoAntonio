04849b336b53b108958da995895e53ef3d0d4396
EliseoAntonio <89654953+EliseoAntonio@users.noreply.github.com>
2021-08-29 12:40:17 -0500

    10 Terminado
    Buenas tardes prof

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    #GameStore tiene venta de videojuegos los nuevos tienen un costo de 1,000 y los usados 350. 
+    #Escribe un programa que sirva para calcular el total de la compra. 
+
+    nuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    viejos = int(input("Dame la cantidad de juegos usados: "))
+    #Proceso a hacer las operaciones correspondientes
+    total = (nuevos * 1000) + (viejos * 350)
+    print("El total de la compra es:", total)
+
 
 
 if __name__ == '__main__':
