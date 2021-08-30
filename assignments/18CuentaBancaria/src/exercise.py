cec31c636f0af62f76f9d42460a35acac8818b3a
EliseoAntonio <89654953+EliseoAntonio@users.noreply.github.com>
2021-08-29 20:48:05 -0500

    Actividad 18 Terminada Funcional

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,7 +1,21 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
-
+    # Un banco cobra el 7.5% de interés mensual sobre 
+    # el saldo final de una cuenta. Además, cada cheque 
+    # expedido tiene un costo de 13 pesos. Realiza un 
+    # programa para obtener el saldo mensual de una cuenta 
+    # en este banco tomando en cuenta el saldo del mes anterior,
+    # los ingresos, los egresos y el número de cheques expedidos.
+    
+    saldoA = float(input("Dame el saldo del mes anterior: "))
+    ingre = float(input("Dame los ingresos: "))
+    egre = float(input("Dame los egresos: "))
+    cheques = int(input("Dame el numero de cheques: "))
+    #operacion
+    SinI = ((saldoA + ingre) - (egre + (cheques * 1)))
+    interes = SinI / 7.5
+    total = SinI - interes
+    #salida
+    print("El saldo final de la cuenta es:", total)
 
 if __name__ == '__main__':
     main()
